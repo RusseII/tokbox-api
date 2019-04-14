ad6efb5b339f803ab92f8677bdcd7e16a4166340
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-04-13 20:32:37 -0400

    currently deployed

--- a.js
+++ a.js
@@ -7,40 +7,40 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var cors = require('cors');
-var app = express();
+var a = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+a.set('views', path.join(__dirname, 'views'));
+a.set('view engine', 'jade');
 
-app.use(cors());
+a.use(cors());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// a.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+a.use(logger('dev'));
+a.use(bodyParser.json());
+a.use(bodyParser.urlencoded({ extended: false }));
+a.use(cookieParser());
+a.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+a.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+a.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+a.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.a.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports = a;
